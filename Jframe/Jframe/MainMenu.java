d2707746f5f9f1d182457695adf9fdcd058f951a
JiangLu <jnsmsjianglu@126.com>
2016-09-30 14:31:26 +0800

    stiithihhh this version is modified by XueFeng Kuang,but this version has also problem on customers Vector,so I need to modify more,hoevwer the version just satisfy most of the requirements!

---
+++ Jframe/Jframe/MainMenu.java
@@ -0,0 +1,106 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class MainMenu extends JFrame implements ActionListener  
+{
+
+    JButton findCustomerButton,addCustomerButton,closeButton;
+    static MainMenu frame;
+	public static void main(String[] args) 
+	{
+		frame = new MainMenu();
+		frame.setVisible(true);
+	}
+
+	public MainMenu()
+	{   
+		Container c = this.getContentPane();
+		c.setLayout(new GridLayout(2,1));
+		JPanel lowerPanel = new JPanel(new FlowLayout());
+        
+		Font defaultFont = c.getFont();
+		
+		JLabel logoLabel = new JLabel(" ",SwingConstants.CENTER);
+		logoLabel.setForeground(Color.red);
+		logoLabel.setFont(new Font("TimesRoman",Font.ITALIC,36));
+		logoLabel.setText("BradShaw Marina");
+		c.add(logoLabel);
+
+		findCustomerButton = new JButton("Find a Customer");
+		addCustomerButton = new JButton("Add a Customer");
+		closeButton = new JButton("Close");
+
+		lowerPanel.add(findCustomerButton);
+		lowerPanel.add(addCustomerButton);
+		lowerPanel.add(closeButton);
+		c.add(lowerPanel);
+
+		this.setSize(320,200);
+		this.setTitle("MainMenu");
+
+		Customer.initialize();//初始化数据库；
+
+		findCustomerButton.addActionListener(this);
+		addCustomerButton.addActionListener(this);
+		closeButton.addActionListener(this);
+		this.addWindowListener(
+			new WindowAdapter()
+		{
+			public void  WindowClosing(WindowEvent event)
+			{
+				shutDown();
+			}
+		});
+
+		
+	}
+
+
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource() == findCustomerButton)
+		{
+			findCustomer();
+		}
+		if (e.getSource() == addCustomerButton)
+		{
+			addCustomer();
+		}
+		if (e.getSource() == closeButton)
+		{
+			shutDown();
+		}
+	}
+
+	public void shutDown()
+	{
+		this.dispose();
+	    System.exit(0);
+	}
+
+	public void findCustomer()
+	{
+		System.out.println("findCustomer...");
+		this.setVisible(false);
+		FindCustomer findCustomerFrame = new FindCustomer(this);
+		findCustomerFrame.setSize(320,200);
+		findCustomerFrame.setTitle("Find A Customer");
+		findCustomerFrame.setVisible(true);
+		
+	}
+
+	public void addCustomer()
+	{
+		System.out.println("addCustomer...");
+		this.setVisible(false);
+		AddCustomer addCustomerFrame = new AddCustomer(this);
+		addCustomerFrame.setSize(320,200);
+		addCustomerFrame.setTitle("Add A Customer");
+		addCustomerFrame.setVisible(true);
+		
+	}
+
+}
+
+
+
